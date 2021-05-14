5bc03eeb94dd32fe1bb4fc1dacda4118b0bcaf42
Roma Pleshenko <romaple28@gmail.com>
2021-05-14 14:22:11 +0300

    Update num.js

--- num.js
+++ num.js
@@ -2,5 +2,17 @@ let a = prompt('введите свой возраст  ');
 if (isNaN(a)) {
   alert('Нужно писать число!');
 } else {
-  alert(a+' '+'Лет');
+  if (a[a.length-1]==0,5<=a[a.length-1]<=9){
+    alert(a+' '+'Лет'); 
+  }
+   else{
+    if (a[a.length-1]==1){
+      alert(a+' '+'Год'); 
+    }
+    else {
+      if (2<=a[a.length-1]<=4){
+        alert(a+' '+'Года'); 
+      }
+    }
+   }  
 }
