2b394585b034e034ed2f548d5b206160b3483072
Roma Pleshenko <romaple28@gmail.com>
2021-05-16 17:02:53 +0300

    Update num.js

--- num.js
+++ num.js
@@ -1,7 +1,7 @@
 let age = +prompt('Введите ваш возраст:')
 let message;
 		if(age !== age) {
-			message = 'Вы ввели невалидное значение';
+			message = 'Введите число';
 		}
 		else if (age % 10 === 0 || age % 10 >= 5 ||  age % 100 >= 5 && age % 100 <= 14 ) {
 			message = `${age} лет`;
